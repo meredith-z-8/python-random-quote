6d03e0e020e5582fa08ff2123ebd5b9ac04109da
meredith-z-8 <xz2944@columbia.edu>
2021-07-21 21:06:36 +0800

    Multiple quotes printed on the same line

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,5 @@
 import random
+
 def primary():
   #print("Keep it logically awesome.")
 
@@ -7,7 +8,9 @@ def primary():
   f.close()
   last = len(quotes)-1
   rnd = random.randint(0,last)
-  print(quotes[rnd])
+  rnd2 = random.randint(0,last)
+  print(quotes[rnd][:len(quotes[rnd])-1], quotes[rnd2][:len(quotes[rnd2])-1], sep = ' and ')
+  
 
 if __name__== "__main__":
   primary()
